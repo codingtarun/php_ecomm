6283b89f9734f39911ac6657f70c7971cd480967
codingtarun <coding.tarun@gmail.com>
2023-04-24 18:51:36 +0530

    Initial commit

---
+++ app/database/connection.php
@@ -0,0 +1,3 @@
+<?php
+
+$conn = mysqli_connect("localhost:3308", "root", "bs09Btcs183@", "php_ecomm_db") or die("Error connecting database");
---
+++ db.sql
@@ -0,0 +1,4 @@
+-- Creating Database
+CREATE SCHEMA `php_ecomm_db` ;
+
+-- Creating Tables 
